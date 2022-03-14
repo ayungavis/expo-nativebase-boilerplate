7f6a75ff5d91aacd9e0a17013c90e988ad57da73
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2022-03-14 22:39:24 +0700

    🎨  style: update style using native base

--- src/screens/root.screen.tsx
+++ src/screens/root.screen.tsx
@@ -1,30 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Container, Heading, Text } from "native-base";
 import { RootStackScreenProps } from "../types/navigation.types";
 
 const RootScreen = ({}: RootStackScreenProps<"Root">): JSX.Element => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Root screen</Text>
-      <View style={styles.separator} />
-    </View>
+    <Container flex={1} alignItems="center" justifyContent="center" mx="auto">
+      <Heading>Root screen</Heading>
+      <Text mt="3" fontWeight="medium" textAlign="center">
+        Edit `src/screens/root.screen.tsx` and save to reload.
+      </Text>
+    </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
 
 export default RootScreen;
