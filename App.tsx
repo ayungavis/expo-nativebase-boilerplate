e8f4775d348ddf8c3f3935a0173220e741b0df14
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2022-03-15 01:21:02 +0700

    ♻️  refactor: linted and prettied

--- App.tsx
+++ App.tsx
@@ -1,27 +1,27 @@
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import Navigation from "./src/navigation";
-import useCachedResources from "./src/hooks/use-cached-resources";
-import { NativeBaseProvider } from "native-base";
-import { Provider } from "react-redux";
-import { store, persistor } from "./src/store/store";
-import { PersistGate } from "redux-persist/integration/react";
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NativeBaseProvider } from 'native-base';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import Navigation from './src/navigation';
+import useCachedResources from './src/hooks/use-cached-resources';
+import { store, persistor } from './src/store/store';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
 
   if (!isLoadingComplete) {
     return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <NativeBaseProvider>
-              <Navigation />
-            </NativeBaseProvider>
-          </PersistGate>
-        </Provider>
-      </SafeAreaProvider>
-    );
   }
+  return (
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <NativeBaseProvider>
+            <Navigation />
+          </NativeBaseProvider>
+        </PersistGate>
+      </Provider>
+    </SafeAreaProvider>
+  );
 }
